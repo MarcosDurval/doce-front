58133b141554449d205b38b0bd95f0ead74fe68d
marcos <marc0sdurval20@gmail.com>
2022-09-13 17:31:39 -0300

    test: update help render

--- __tests__/login/login.spec.tsx
+++ __tests__/login/login.spec.tsx
@@ -10,18 +10,14 @@ describe("Login", () => {
     mockAxios.reset();
   });
 
-  it("should renders the inputs", () => {
-    // arrange
-    renderWithRouter(<Login />);
+  it("should renders the inputs", async () => {
+    renderWithRouter(<Login />, { route: "/login" });
 
-    // act
     const username = screen.getByLabelText("Usuário:") as HTMLInputElement;
     const password = screen.getByLabelText("Senha:") as HTMLInputElement;
 
     fireEvent.change(username, { target: { value: "name" } });
     fireEvent.change(password, { target: { value: "password" } });
-
-    // assert
 
     expect(username?.value).toBe("name");
     expect(password?.value).toBe("password");
@@ -29,46 +25,40 @@ describe("Login", () => {
     expect(password).toHaveAttribute("required");
   });
   it("it should faill login action", async () => {
-    renderWithRouter(<Login />);
-
     const error = { response: { status: 401 } };
     mockAxios.post.mockRejectedValueOnce(error);
+
+    const { user } = renderWithRouter(<Login />, { route: "/login" });
+
     const username = screen.getByLabelText("Usuário:");
     const password = screen.getByLabelText("Senha:");
     const button = screen.getByText("Entrar");
 
-    fireEvent.change(username, { target: { value: "name" } });
-    fireEvent.change(password, { target: { value: "password" } });
+    user.type(username, "nome");
+    user.type(password, "senha");
 
-    fireEvent.click(button);
+    user.click(button);
     await waitFor(async () => {
       const failLogin = await screen.findByText("Usuário ou senha inválidos");
       expect(failLogin).toBeInTheDocument();
     });
   });
   it("it should be possible to log in", async () => {
-    Object.defineProperty(window, "localStorage", {
-      value: {
-        getItem: jest.fn(() => null),
-        setItem: jest.fn(() => null),
-      },
-      writable: true,
-    });
-    renderWithRouter(<Login />);
-
     const response = { data: { access: "token" } };
-
     mockAxios.post.mockResolvedValueOnce(response);
+
+    const { user } = renderWithRouter(<Login />, { route: "/login" });
 
     const username = screen.getByLabelText("Usuário:");
     const password = screen.getByLabelText("Senha:");
     const button = screen.getByText("Entrar");
 
-    fireEvent.change(username, { target: { value: "name" } });
-    fireEvent.change(password, { target: { value: "password" } });
+    user.type(username, "name");
+    user.type(password, "password");
 
-    fireEvent.click(button);
-    const failLogin = screen.queryByText("Usuário ou senha inválidos");
-    expect(failLogin).not.toBeInTheDocument();
+    user.click(button);
+    await waitFor(async () => {
+      expect(window.location.pathname).toBe("/produtos");
+    });
   });
 });
