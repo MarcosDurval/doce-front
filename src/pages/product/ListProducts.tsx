1ad47c826b6254a11d395e97d3253aedf235c65a
marcos <marc0sdurval20@gmail.com>
2022-09-16 11:40:16 -0300

    feat: created  CardProduct component

--- src/pages/product/ListProducts.tsx
+++ src/pages/product/ListProducts.tsx
@@ -2,11 +2,13 @@ import "@/styles/pages/products.scss";
 
 import { useEffect, useState } from "react";
 
+import { CardProduct } from "@/components/CardProduct";
 import Header from "@/components/Header";
 import api from "@/services/api";
 
 const ListProducts = () => {
   const [products, setProducts] = useState([]);
+  const [load, setLoad] = useState<boolean>(true);
   const page = window.location.pathname;
   useEffect(() => {
     api
@@ -16,17 +18,25 @@ const ListProducts = () => {
       })
       .catch(error => {
         console.log(error);
-      });
+      })
+      .finally(() => setLoad(false));
   }, []);
+
+  if (load) {
+    return <h1>Carregando...</h1>;
+  }
+
   return (
     <>
       <main className="main-pd">
         <Header page={page} setList={setProducts} />
-        {products.length > 0
-          ? products.map((_product, index) => {
-              return <h1 key={index}>olá mundo</h1>;
-            })
-          : null}
+        {products.length > 0 ? (
+          <div>
+            {products.map((product, index) => {
+              return <CardProduct key={index} results={product} />;
+            })}
+          </div>
+        ) : null}
       </main>
     </>
   );
