ac3051054692532ca05ba80c3e3cf3c7c06ec84e
marcos <marc0sdurval20@gmail.com>
2022-10-21 17:41:51 -0300

    test: create supplie

--- __tests__/supplies/supplies.spec.tsx
+++ __tests__/supplies/supplies.spec.tsx
@@ -104,17 +104,52 @@ describe("Page Supplies", () => {
     mockAxios.get.mockResolvedValueOnce(responseEdit);
 
     await waitFor(async () => {
-      const insumo = await screen.findByLabelText("Insumo:");
+      const supplie = await screen.findByLabelText("Insumo:");
       const button = await screen.findByRole("button", {
         name: "Atualizar Insumo",
       });
-      user.type(insumo, "Abacaxi");
+      user.type(supplie, "Abacaxi");
       user.click(button);
     });
 
     await waitFor(async () => {
-      const insumo = await screen.findByText(/Abacaxi$/);
-      expect(insumo).toBeInTheDocument();
+      const supplie = await screen.findByText(/Abacaxi$/);
+      expect(supplie).toBeInTheDocument();
     });
+  });
+  it("should create supplie", async () => {
+    const response = { data: mockSuppliesRemove };
+    const response1 = { data: mockSupplies };
+    mockAxios.post.mockResolvedValue([]);
+    mockAxios.get.mockResolvedValueOnce(response);
+    const { user } = renderWithRouter(<ListSupplies />, { route: "/insumos" });
+    const btnNew = await screen.findByRole("button", { name: "Novo" });
+
+    await user.click(btnNew);
+
+    const supplie = await screen.findByLabelText("Insumo:");
+    const local = await screen.findByLabelText("Local de compra:");
+    const price = await screen.findByLabelText("Preço:");
+    const uni = await screen.findByLabelText("Unidade de medida:");
+    const date = await screen.findByLabelText("Data:");
+    const quantity = await screen.findByLabelText("Quantidade da embalagem:");
+    const btnCreate = await screen.findByRole("button", {
+      name: "Cadastrar",
+    });
+
+    mockAxios.get.mockReset();
+    mockAxios.get.mockResolvedValueOnce(response1);
+
+    await user.type(supplie, "abacaxi");
+    await user.type(local, "atacadao");
+    await user.type(price, "3.75");
+    await user.type(date, "2020-04-01");
+    await user.type(quantity, "1000");
+    await user.type(uni, "Gramas");
+
+    await user.click(btnCreate);
+
+    const allSupplies = await screen.findAllByTestId("card-supplie");
+    expect(allSupplies).toHaveLength(9);
   });
 });
